efd43c41a08f82c039e14453c3a3a941662385d9
Nicolas CLERC <Nicolas CLERC>
2018-02-13 23:53:47 +0100

    correct bit file size

--- logi_loder_x86/Program.cs
+++ logi_loder_x86/Program.cs
@@ -9,12 +9,15 @@ namespace logi_loder_x86
         static void Main(string[] args)
         {
             Console.WriteLine("LOGI_LOADER VERSION : 0.1.0");
-            Console.WriteLine("Compiled for LOGI-PI x86");
+            Console.WriteLine("Compiled for LOGI-PI SIMULx86");
             Console.WriteLine("Board variant is LOGIPI_R1.5");
 
             if (args.Length==1 && System.IO.File.Exists(args[0]))
             {
-                Console.WriteLine("BIT file found.");
+                var fi = new System.IO.FileInfo(args[0]);
+                Console.WriteLine($"bit file size : {fi.Length}");
+                System.Threading.Thread.Sleep(2000); // simulate FPGA burn time
+                Console.WriteLine("bitstream loaded, check done led");
             }
             else // simulate the bug of logi_loader launch without parameters or with a wrong filename
                 Console.Error.WriteLine("Segmentation fault"); 
